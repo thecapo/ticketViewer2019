738eaf55c47cbe903ac5a1d8ca2998396d102c54
thecapo <the7thcapo@gmail.com>
2019-06-05 16:01:04 +1000

    added test with no tickets

--- spec/features/tickets_spec.rb
+++ spec/features/tickets_spec.rb
@@ -9,31 +9,23 @@ RSpec.feature "Tickets", :vcr, type: :feature do
       expect(page).to have_content('Subject')
       expect(page).to have_css('.pagination')
       expect(page).to have_css('.current')
-      # expect(page).to have_css('.previous_page')
-      # expect(page).to have_selector(:css, 'a[href="/tickets?page=2"]')
-      # expect(page).has_link('a[href]')
+      # should add click show link
+     
     end
 
     scenario 'no tickets to show' do
-      
+      visit root_path
+      expect(page).to have_content('Oooops, No Tickets Found')
     end
 
-    # scenario 'should not show tickets if api is down' do
-    #   visit root_path
-    #   expect(response).to have_http_status(200)
-    # end
   end
 
   context 'show individual ticket' do
     scenario 'should show an individual ticket if it exist' do
       visit root_path
+
       expect(page).to have_content('Subject')
-      # find("a[href='/tickets/1']").click_link('Show')  
-      
-    # expect(page).to have_content 'Featured News Article'
-    # expect(page).to_not have_content "No Featured Companies"
-    # expect(page).to_not have_content 'Edit Featured Company'
-      # expect(page).has_css?('#ticketID')
+   
     end
     scenario 'ticket does not exist' do
     end
