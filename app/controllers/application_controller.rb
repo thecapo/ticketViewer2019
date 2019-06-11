889d1833dfa42bd46b9fa1ecf9bed9aec088d81c
thecapo <the7thcapo@gmail.com>
2019-06-11 21:24:32 +1000

    removed unecessary error catching

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  rescue_from ActionController::RoutingError do |exception|
-		logger.error 'Routing error occured' 
-		render file: "#{Rails.root}/public/404.html" , status: :not_found  
-	end
-
-	rescue_from ActionController::UnknownFormat do |exception|
-		logger.error 'Unknown Format'
-		render file: "#{Rails.root}/public/422.html" , status: :not_found  
-	end
 end
