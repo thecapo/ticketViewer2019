3428664d6c6c77b586683989fb5c4c74eb1e570e
thecapo <the7thcapo@gmail.com>
2019-06-11 18:02:59 +1000

    refactored tickets controller

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,31 +1,13 @@
 class TicketsController < ApplicationController
   require 'will_paginate/array'
-		  
-  def index
-    # gets tickets from api
-    response = HTTParty.get('https://the7thcapo18.zendesk.com/api/v2/tickets.json', basic_auth: set_ticket, :headers => {'Content-Type' => 'application/json'} )
-    @ticketsParsed = response.parsed_response["tickets"]
-    @tickets = @ticketsParsed.paginate(:page => params[:page], :per_page => 25)
-  end
 
-  def next_page
-    response = HTTParty.get('https://the7thcapo18.zendesk.com/api/v2/tickets?page=2', basic_auth: set_ticket, :headers => {'Content-Type' => 'application/json'} )
-    @ticketsParsed = response.parsed_response["tickets"]
-    @tickets = @ticketsParsed.paginate(:page => params[:page], :per_page => 25)
-  end
-
+  before_action :api_connect, only: [:index, :next_page]
+  before_action :one_ticket, only: [:show]
+  
   def show
-    # gets the specific ticket
-    @urlTicket = "https://the7thcapo18.zendesk.com/api/v2/tickets/#{(params[:id])}" 
-    @response = HTTParty.get(@urlTicket, basic_auth: set_ticket, :headers => {'Content-Type' => 'application/json'} )
-    @showTicket = @response.parsed_response["ticket"]
-
-  if @showTicket.nil?
-    render file: "#{Rails.root}/public/404.html" , status: :not_found  
-  else
-    @showTicket
-  end 
-
+    if @showTicket.nil?
+      render file: "#{Rails.root}/public/404.html" , status: :not_found  
+    end 
   end 
 
   def catch_404
@@ -41,6 +23,20 @@ class TicketsController < ApplicationController
   end
 
   private
+
+  def api_connect
+    # gets all tickets
+    response = HTTParty.get("https://the7thcapo18.zendesk.com/api/v2/tickets?page=#{(params[:ticket_id])}", basic_auth: set_ticket, :headers => {'Content-Type' => 'application/json'} ) 
+    @ticketsParsed = response.parsed_response["tickets"]
+    @tickets = @ticketsParsed.paginate(:page => params[:page], :per_page => 25)
+  end
+
+  def one_ticket
+    # gets the specific ticket
+    @urlTicket = "https://the7thcapo18.zendesk.com/api/v2/tickets/#{(params[:id])}" 
+    @response = HTTParty.get(@urlTicket, basic_auth: set_ticket, :headers => {'Content-Type' => 'application/json'} )
+    @showTicket = @response.parsed_response["ticket"]
+  end
 
   def set_ticket
     auth = {username: "the7thcapo@gmail.com", password: "DesktopZEN1"}
