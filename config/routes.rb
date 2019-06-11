9eb051edb954405d79d85740d258337d9c7a1474
thecapo <the7thcapo@gmail.com>
2019-06-11 21:04:48 +1000

    added a new route and only show index and show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :tickets do
-    get '/next_page', to: 'tickets#next_page'
+  resources :tickets, only: [:index, :show] do
+    get '/next_page', to: 'tickets#index'
   end
 	root 'tickets#index'
   match "*path", to: "tickets#catch_404", via: :all
